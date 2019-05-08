16f14e30c6f7d8743ed5e6b79fd7ea0ff8096333
Calvin <cabagbyii@gmail.com>
2019-05-08 02:55:09 -0400

    added basis for subscribe plugin

---
+++ sbscrbr.js
@@ -0,0 +1,11 @@
+(function(){
+	//private variables
+	var privateVar = "John Cena"
+
+	//constructor
+	this.Sbscrbr = function(){
+		console.log('shits and giggles');
+	}
+}());
+
+var currSbscrbr = new Sbscrbr();
